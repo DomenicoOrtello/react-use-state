c06921c38faf3ace430923550e21ecf5dfa2940d
Ortello7 <domenicoortello00@gmail.com>
2025-02-07 10:22:22 +0100

    end project

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -1,11 +1,21 @@
-return (
-  <div className="language">
-    <div className="language-title">
-      <h1>{title}</h1>
-      <button className="btn" onClick={toggle}>
-          {isOpen ? "-" : "+"}
-      </button>
+import { useState } from "react";
+
+export default function Language({ title, description }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen((current) => !current)
+  };
+
+  return (
+    <div className="language">
+      <div className="language-title" style={{backgroundColor: isOpen ? "#ecc53e" : "white"}}>
+        <h1><strong>{title}</strong></h1>
+        <button style={{backgroundColor: isOpen ? "#ecc53e" : "white"}} className="btn" onClick={toggle}>
+        {isOpen ? "-" : "+"}
+        </button>
+        </div>
+      <div className="language-content">{isOpen && <p>{description}</p>}</div>
     </div>
-    <div className="language-content">{isOpen && <p>{description}</p>}</div>
-  </div>
-);
+  );
+} 
